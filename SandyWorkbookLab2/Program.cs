558734aa2ce888bd20fff806ce47611e636320a9
Sandy Jurek <sandyjurek@quickenloans.com>
2020-07-11 12:11:34 -0400

    Add project files.

---
+++ SandyWorkbookLab2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SandyWorkbookLab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the width of the classroom in inches? ");
+            string input1 = Console.ReadLine();
+            int num1 = int.Parse(input1);
+
+            Console.Write("What is the length of the classroom in inches? ");
+            string input2 = Console.ReadLine();
+            int num2 = int.Parse(input2);
+
+
+        }
+    }
+}
