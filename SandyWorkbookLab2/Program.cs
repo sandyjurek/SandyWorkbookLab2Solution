e5cf0c4a3a5e45f3539d10de6966f26c2aa99c23
Sandy Jurek <sandyjurek@quickenloans.com>
2020-07-13 11:27:12 -0400

    major changes

--- SandyWorkbookLab2/Program.cs
+++ SandyWorkbookLab2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection.Metadata.Ecma335;
 
 namespace SandyWorkbookLab2
 {
@@ -6,15 +7,27 @@ namespace SandyWorkbookLab2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is the width of the classroom in inches? ");
-            string input1 = Console.ReadLine();
-            int num1 = int.Parse(input1);
+            string input3;
 
-            Console.Write("What is the length of the classroom in inches? ");
-            string input2 = Console.ReadLine();
-            int num2 = int.Parse(input2);
+            do
+            {
+                Console.WriteLine("What is the width? (ie., 8 feet and 6 inches enter as 8.6)");
+                decimal input1 = Convert.ToDecimal(Console.ReadLine());
 
+                Console.WriteLine("What is the length?");
+                decimal input2 = Convert.ToDecimal(Console.ReadLine());
 
+                decimal sumArea = input1 * input2;
+                decimal sumPeri = 2 * (input1 + input2);
+
+                Console.WriteLine(sumArea + " Square Feet is the Area");
+                Console.WriteLine(sumPeri + " Feet is the Perimeter");
+
+                Console.WriteLine("Would you like to continue (yes/no)?");
+                input3 = Console.ReadLine();
+            }
+            while (input3 == "yes");
+            return;
         }
     }
 }
